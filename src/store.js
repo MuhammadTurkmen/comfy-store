f79d046017171d4b4c1633cee33b5ee5511396fe
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-01-01 08:36:44 +0430

    init

--- src/store.js
+++ src/store.js
@@ -3,7 +3,7 @@ let store = [];
 const setupStore = (products) => {
     store = products.map((product) => {
         const {id, fields:{featured, name, price, company, colors, image: img}} = product
-        const image = img[0].thumbnails
+        const image = img[0].thumbnails.large.url
         return {id, featured, name, price, company, colors}
     })
 };
