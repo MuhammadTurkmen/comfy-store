08dfb7308f7cab920c0e9e48ef1d64225f9a5f2c
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-01-01 08:37:44 +0430

    init

--- src/store.js
+++ src/store.js
@@ -4,7 +4,7 @@ const setupStore = (products) => {
     store = products.map((product) => {
         const {id, fields:{featured, name, price, company, colors, image: img}} = product
         const image = img[0].thumbnails.large.url
-        return {id, featured, name, price, company, colors}
+        return {id, featured, name, price, company, colors, image}
     })
 };
 const findProduct = () => {};
