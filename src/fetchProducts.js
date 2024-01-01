2a74b897744e9ebcf9cf0af12e58ef92bf5994a7
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-01-01 08:12:23 +0430

    init

--- src/fetchProducts.js
+++ src/fetchProducts.js
@@ -1,5 +1,7 @@
 import { allProductsUrl } from './utils.js';
 
-const fetchProducts = async () => {};
+const fetchProducts = async () => {
+    const response = await fetch(allProductsUrl).catch(err => console.log(err);) 
+};
 
 export default fetchProducts;
