ebdce8523cca50534a0124b3187719961bdcad40
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-01-01 08:33:49 +0430

    init

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import { getStorageItem, setStorageItem } from './utils.js';
 let store = [];
 const setupStore = (products) => {
     store = products.map((product) => {
-        const {id, fields:{featured, name, price, company, colors, image}} = product
+        const {id, fields:{featured, name, price, company, colors, image: img}} = product
         return {id,}
     })
 };
