24b255e9a1932217753ac7b7bbf3a122b9917bce
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-01-02 11:29:54 +0430

    init

--- src/displayProducts.js
+++ src/displayProducts.js
@@ -4,7 +4,24 @@ const display = (products, element) => {
     // display products
     element.innerHTML = products.map((product) => {
         const {id, name, image, price} = product
-        return ``
+        return `<!-- <article class="product">
+                        <div class="product-container">
+                            <img src="./images/main-bcg.jpeg" class="product-img img" alt="">
+
+                            <div class="product-icons">
+                                <a href="product.html?id=1" class="product-icon">
+                                <i class="fas fa-search"></i>
+                                </a>
+                                <button class="product-cart-btn product-icon" data-id="1">
+                                <i class="fas fa-shopping-cart"></i>
+                                </button>
+                            </div>
+                        </div>
+                        <footer>
+                            <p class="product-name">name</p>
+                            <h4 class="product-price">$9.99</h4>
+                        </footer>
+                    </article> -->`
     })
 };
 
