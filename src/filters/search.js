a57eb26514616d1566b8365bd7c5d8a55a61b055
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-01-02 14:45:10 +0430

    init

--- src/filters/search.js
+++ src/filters/search.js
@@ -3,6 +3,9 @@ import display from '../displayProducts.js';
 const setupSearch = (store) => {
     const form = getElement('.input-form')
     const nameInput = getElement('.search-input')
+    form.addEventListener('keyup', function() {
+        
+    })
 };
 
 export default setupSearch;
