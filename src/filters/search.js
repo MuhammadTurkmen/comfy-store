f027f8cc59ba847252a4c1e49f4d14de2d33ce02
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-01-02 14:40:40 +0430

    init

--- src/filters/search.js
+++ src/filters/search.js
@@ -1,5 +1,7 @@
 import { getElement } from '../utils.js';
 import display from '../displayProducts.js';
-const setupSearch = (store) => {};
+const setupSearch = (store) => {
+    const form = getElement('.input-form')
+};
 
 export default setupSearch;
