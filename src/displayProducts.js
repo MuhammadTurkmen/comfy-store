48ccb960a362ed657eb55401cbdddd05e85f2465
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-01-02 11:18:59 +0430

    init

--- src/displayProducts.js
+++ src/displayProducts.js
@@ -1,5 +1,5 @@
 import { formatPrice } from './utils.js';
 import { addToCart } from './cart/setupCart.js';
-const display = () => {};
+const display = (products, element) => {};
 
 export default display;
