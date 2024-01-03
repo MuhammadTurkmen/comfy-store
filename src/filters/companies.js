87a634e00cbbae48770e559cbb6cffc35cbe36ec
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-01-03 20:16:22 +0430

    init

--- src/filters/companies.js
+++ src/filters/companies.js
@@ -2,7 +2,8 @@ import { getElement } from '../utils.js';
 import display from '../displayProducts.js';
 
 const setupCompanies = (store) => {
-    let companies = ['all'...new Set(store.map((product) => product.company))]
+    let companies = ['all',...new Set(store.map((product) => product.company))]
+    const companiesDOM = getElement('.companies')
 };
 
 export default setupCompanies;
