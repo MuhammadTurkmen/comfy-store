61915b4fa360fc31736ad3cc2bf7312697721f6c
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-01-03 18:13:17 +0430

    init

--- src/filters/search.js
+++ src/filters/search.js
@@ -9,7 +9,7 @@ const setupSearch = (store) => {
             const newStore = store.filter((product) => { 
                 let {name} = product
                 name = name.toLowerCase()
-                if(name.startWith(value)) {
+                if(name.startsWith(value)) {
                     return product
                 }
             }) 
