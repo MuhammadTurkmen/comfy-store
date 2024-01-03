261f5728ced8f1786a76dcf4916ba3869b6eb151
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-01-03 20:06:39 +0430

    init

--- src/filters/companies.js
+++ src/filters/companies.js
@@ -1,6 +1,6 @@
 import { getElement } from '../utils.js';
 import display from '../displayProducts.js';
 
-const setupCompanies = () => {};
+const setupCompanies = (store) => {};
 
 export default setupCompanies;
