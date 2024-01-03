3a9ce5cce78e5ed08b826e0a2b772beb7a953ded
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-01-03 20:36:28 +0430

    init

--- src/filters/price.js
+++ src/filters/price.js
@@ -2,7 +2,7 @@ import { getElement } from '../utils.js';
 import display from '../displayProducts.js';
 
 const setupPrice = (store) => {
-    const priceInput
+    const priceInput = getElement('.price-filter')
 };
 
 export default setupPrice;
