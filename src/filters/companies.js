c838e04bfa80ec84208065118150abab532a3cf2
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-01-03 20:20:01 +0430

    init

--- src/filters/companies.js
+++ src/filters/companies.js
@@ -2,11 +2,11 @@ import { getElement } from '../utils.js';
 import display from '../displayProducts.js';
 
 const setupCompanies = (store) => {
-    let companies = ['all',...new Set(store.map((product) => product.company))]
+    let companies = ['all', ...new Set(store.map((product) => product.company))]
     const companiesDOM = getElement('.companies')
     companiesDOM.innerHTML = companies.map((company) =>  {
-        return `<button class="company-btn">all</button>`
-    })
+        return `<button class="company-btn">${company}</button>`
+    }).join('')
 };
 
 export default setupCompanies;
