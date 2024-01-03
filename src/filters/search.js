d9694fc57d0b3830b7febfc40cf7e5205b65e7cb
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-01-03 18:17:27 +0430

    init

--- src/filters/search.js
+++ src/filters/search.js
@@ -16,6 +16,7 @@ const setupSearch = (store) => {
             display(newStore, getElement('.products-container'))
             if(newStore,length < 1) {
                 const products = getElement('.products-container')
+                products.innerHTML = `<h3 class="filter-error"></h3>`
             }
         }  
         
