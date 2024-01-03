a6479d5c819ad0cdfbf2abe98e5b17b83475b6b6
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-01-03 20:27:31 +0430

    init

--- src/filters/companies.js
+++ src/filters/companies.js
@@ -14,6 +14,10 @@ const setupCompanies = (store) => {
             if(element.textContent === 'all') {
                 newStore = [...store]
             }
+            else{
+
+            }
+            display(newStore, getElement('.products-container'))
         }
     })
 };
