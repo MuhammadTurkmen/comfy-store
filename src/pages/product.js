c09fa04913f3ddd6431b48c8f24f71e58dd3977b
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-01-04 21:46:52 +0430

    init

--- src/pages/product.js
+++ src/pages/product.js
@@ -7,16 +7,16 @@ import { addToCart } from '../cart/setupCart.js';
 import { singleProductUrl, getElement, formatPrice } from '../utils.js';
 
 // selections
-// const loading = getElement('.page-loading');
-// const centerDOM = getElement('.single-product-center');
-// const pageTitleDOM = getElement('.page-hero-title');
-// const imgDOM = getElement('.single-product-img');
-// const titleDOM = getElement('.single-product-title');
-// const companyDOM = getElement('.single-product-company');
-// const priceDOM = getElement('.single-product-price');
-// const colorsDOM = getElement('.single-product-colors');
-// const descDOM = getElement('.single-product-desc');
-// const cartBtn = getElement('.addToCartBtn');
+const loading = getElement('.page-loading');
+const centerDOM = getElement('.single-product-center');
+const pageTitleDOM = getElement('.page-hero-title');
+const imgDOM = getElement('.single-product-img');
+const titleDOM = getElement('.single-product-title');
+const companyDOM = getElement('.single-product-company');
+const priceDOM = getElement('.single-product-price');
+const colorsDOM = getElement('.single-product-colors');
+const descDOM = getElement('.single-product-desc');
+const cartBtn = getElement('.addToCartBtn');
 
 // cart product
 // let productID;
