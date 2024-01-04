09f7a4af11829ec5e77bdc66d45ce777ddee4376
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-01-04 12:33:46 +0430

    init

--- src/store.js
+++ src/store.js
@@ -9,6 +9,7 @@ const setupStore = (products) => {
     setStorageItem('store', store)
 };
 const findProduct = (id) => {
-    let product = store.find(id)
+    let product = store.find((product) => product.id === id) 
+    return product
 };
 export { store, setupStore, findProduct };
