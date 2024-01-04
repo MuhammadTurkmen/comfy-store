d2e2269573bf136ea6efc183e59a11a7147d11cd
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-01-04 22:11:49 +0430

    init

--- src/pages/product.js
+++ src/pages/product.js
@@ -34,7 +34,7 @@ window.addEventListener('DOMContentLoaded', async function() {
             centerDOM.innerHTML = `
                 <div>
                     <h3 class="error">sorry, something went wrong</h3>
-                    <a herf="index.html" class="btn"></a>
+                    <a herf="index.html" class="btn">back home </a>
                 </div>
             `
         }
