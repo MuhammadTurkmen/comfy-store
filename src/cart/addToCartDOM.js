041ffa39cca7e7af64602b8c8c69c4e6e8f3165b
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-01-04 18:04:36 +0430

    init

--- src/cart/addToCartDOM.js
+++ src/cart/addToCartDOM.js
@@ -3,6 +3,7 @@ import { formatPrice, getElement } from '../utils.js';
 const addToCartDOM = ({id, name, price, image, amount}) => {
     const article = document.createElement('article')
     article.classList.add('cart-item')
+    article.setAttribute('data-id', id)
 };
 
 export default addToCartDOM;
