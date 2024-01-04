e7d8e14140c55b5d82d04ef198ae376ea437531e
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-01-04 18:08:03 +0430

    init

--- src/cart/addToCartDOM.js
+++ src/cart/addToCartDOM.js
@@ -4,6 +4,23 @@ const addToCartDOM = ({id, name, price, image, amount}) => {
     const article = document.createElement('article')
     article.classList.add('cart-item')
     article.setAttribute('data-id', id)
+    article.innerHTML = `
+            <img src="./images/main-bcg.jpeg" class="cart-item-img" alt="name">
+            <div>
+              <h4 class="cart-item-name">high-back bench</h4>
+              <p class="cart-item-price">$13.99</p>
+              <button class="cart-item-remove-btn">remove</button>
+            </div>
+            <div>
+              <button class="cart-item-increase-btn">
+                <i class="fas fa-chevron-up"></i>
+              </button>
+              <p class="cart-item-amount">1</p>
+              <button class="cart-item-decrease-btn">
+                <i class="fas fa-chevron-down"></i>
+              </button>
+            </div>
+    `
 };
 
 export default addToCartDOM;
