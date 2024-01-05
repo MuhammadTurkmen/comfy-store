9c3ff0f87d4e9ab3af24f2014c33fcfb3780e29d
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-01-05 22:17:49 +0430

    init

--- src/cart/setupCart.js
+++ src/cart/setupCart.js
@@ -89,6 +89,7 @@ function setupCartFunctionality() {
       // parent.parentElement.remove()
       element.parentElement.parentElement.remove()
     }
+    if(parent.classList.contains('cart-item-increase-btn'))
     // increase
     // decrease
     displayCartItemCount()
