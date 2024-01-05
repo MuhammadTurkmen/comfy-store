68914b5dab2bbf4067d8819fe4cb88b4fb161d22
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-01-05 22:11:49 +0430

    init

--- src/cart/setupCart.js
+++ src/cart/setupCart.js
@@ -63,6 +63,9 @@ function displayCartItemsDOM() {
   })
 }
 
+function removeItem(id) {
+  cart = cart.filter((cartItem) => cartItem.id !== id)
+}
 function increaseAmount(id) {
   let newAmount 
   cart = cart.map((cartItem) => {
