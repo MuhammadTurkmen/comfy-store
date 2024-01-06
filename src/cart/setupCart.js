794c146d736f3f2d083550677c08204d87427613
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-01-06 22:42:56 +0430

    init

--- src/cart/setupCart.js
+++ src/cart/setupCart.js
@@ -109,7 +109,7 @@ function setupCartFunctionality() {
     if(parent.classList.contains('cart-item-decrease-btn')) {
       const newAmount = decreaseAmount(parentID)
       if(newAmount === 0) {
-        removeItem(id)
+        removeItem(parentID) //
         parent.parentElement.parentElement.remove()
       }
       else {
