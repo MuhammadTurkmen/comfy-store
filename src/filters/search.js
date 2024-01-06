c2419e81f965817769c149e83289f89002ee522f
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-01-06 22:38:51 +0430

    init

--- src/filters/search.js
+++ src/filters/search.js
@@ -13,7 +13,7 @@ const setupSearch = (store) => {
                     return product
                 }
             }) 
-            display(newStore, getElement('.products-container'))
+            display(newStore, getElement('.products-container'), true)
             if(newStore.length < 1) {
                 const products = getElement('.products-container')
                 products.innerHTML = `<h3 class="filter-error">
@@ -23,7 +23,7 @@ const setupSearch = (store) => {
         }  
         
         else { 
-            display(store, getElement('.products-container'))
+            display(store, getElement('.products-container'), true)
         } 
     })
 };
