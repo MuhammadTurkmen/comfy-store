55d3e171abdaa77bd63796931421dbef60786ff9
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-01-08 20:52:57 +0430

    init

--- src/toggleSidebar.js
+++ src/toggleSidebar.js
@@ -1,4 +1,8 @@
 import { getElement } from './utils.js';
 const toggleNave = getElement('.toggle-nav')
 const sidebarOverlay = getElement('.sidebar-overlay')
-const closeBtn
+const closeBtn = getElement('.sidebar-close')
+
+toggleNave.addEventListener('click', () => {
+    sidebarOverlay.classList.add('show')
+})
