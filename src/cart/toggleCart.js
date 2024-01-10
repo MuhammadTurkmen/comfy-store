ddc173bbd9a8c74eadd6150c862be6072475cd5a
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-01-10 21:28:14 +0430

    init

--- src/cart/toggleCart.js
+++ src/cart/toggleCart.js
@@ -6,7 +6,7 @@ const toggleCartBtn = getElement('.toggle-cart')
  
 toggleCartBtn.addEventListener('click', () => {
     cartOverlay.classList.add('show')
-})
+}) 
 closeCartBtn.addEventListener('click', () => { 
     cartOverlay.classList.remove('show')
 })
