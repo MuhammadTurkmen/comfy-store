47ac70adc6c0c67d980742d307f106610255bca8
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-01-10 21:28:05 +0430

    init

--- src/cart/toggleCart.js
+++ src/cart/toggleCart.js
@@ -3,7 +3,7 @@ import { getElement } from '../utils.js';
 const cartOverlay = getElement('.cart-overlay')
 const closeCartBtn = getElement('.cart-close')
 const toggleCartBtn = getElement('.toggle-cart')
-
+ 
 toggleCartBtn.addEventListener('click', () => {
     cartOverlay.classList.add('show')
 })
