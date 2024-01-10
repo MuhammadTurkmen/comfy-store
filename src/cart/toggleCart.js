7560dd15b0920bd10a231736ddcaa5c8319cc506
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-01-10 21:19:48 +0430

    init

--- src/cart/toggleCart.js
+++ src/cart/toggleCart.js
@@ -1,7 +1,7 @@
 import { getElement } from '../utils.js';
 
 const cartOverlay = getElement('.cart-overlay')
-const cartOverlay = getElement('.cart-overlay')
+const closeCartBtn = getElement('.cart-close')
 const cartOverlay = getElement('.cart-overlay')
 
 export const openCart = () => {};
