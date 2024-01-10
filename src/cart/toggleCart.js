addbc86d3454a7c1578798a15177817e2f4d15ab
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-01-10 21:19:09 +0430

    init

--- src/cart/toggleCart.js
+++ src/cart/toggleCart.js
@@ -1,3 +1,7 @@
 import { getElement } from '../utils.js';
 
+const cartOverlay = getElement('.cart-overlay')
+const cartOverlay = getElement('.cart-overlay')
+const cartOverlay = getElement('.cart-overlay')
+
 export const openCart = () => {};
